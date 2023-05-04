073a8c0bf6a2b744acaba6df599e7a80cb76a9ef
T Savo <evilgenius@nefariousplan.com>
2023-05-04 12:28:14 -0700

    Initial commit

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
     author_email="evilgenius@nefariousplan.com",
     description="A library for transforming unstructured text into structured data without context/mappings using ChatGPT.",
     long_description="A library for transforming unstructured text into structured data without context/mappings using ChatGPT.",
-    keywords="openai gpt3 chatgpt nlp chatbot transformers structruing text"
+    keywords="openai gpt3 chatgpt nlp chatbot transformers structruing text",
+    url="https://github.com/TSavo/transformgpt",
+    requires=["openai"],
 )
